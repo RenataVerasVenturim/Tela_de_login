e470189d88764f31eb10b512ec133d11d2d95a38
Renata Veras Venturim <129551549+RenataVerasVenturim@users.noreply.github.com>
2023-07-01 10:53:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ function verificar_acesso(){
     var senha;
     /*Entrada de dados*/
     
-    email=document.f_login.f_email.value;
-    senha=document.f_login.f_senha.value;
-
+    email=document.f_login.f_email.value.toUpperCase();
+    senha=document.f_login.f_senha.value;       
+    
     /*Saída de dados*/
         
-        if (email !== 'renata_venturim@hotmail.com'){
+        if (email !== 'RENATA_VENTURIM@HOTMAIL.COM'){
+            email=document.f_login.f_email.value;
         
             window.alert('E mail não encontrado e/ou senha incorreta');
         }
